ea667907d5d7ccaa417233b4f18727852bc858a3
Samuel Haselvander <samuelhaselvander33@gmail.com>
2022-01-17 15:39:36 +0100

    implements popUp markers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let menuIcon = document.getElementById("Menu_icon");
+let markerTab = [];
+let titleTab = [];
+let descTab = [];
 let firstEnter = true;
 let click = false;
 var mymap = L.map('mapid').setView([47.46667, -0.55], 13);
 var popup = L.popup();
-var marker;
+var marker = 0;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -15,13 +18,25 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 function onMapClick(e) {
+    let title;
+    let desc;
     if(click){
-        /*popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);*/
-        console.log(e.latlng.lat);
         marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);   //marker.latlng.lat
+        markerTab.push(marker);
+        title = window.prompt("Saisissez un titre à votre évènement");
+        titleTab.push(title);
+        desc = window.prompt("Saisissez la description de votre évènement");
+        descTab.push(desc);
+    }
+    if(marker != 0){
+        marker.addEventListener("click", function(){
+            console.log("marker clicked");
+            for(let i = 0; i < markerTab.length; i++){
+                if(e.latlng.lat == markerTab[i]._latlng.lat && e.latlng.lng == markerTab[i]._latlng.lng){
+                    markerTab[i].bindPopup("<b>"+titleTab[i]+"</b><br>"+descTab[i]+".").openPopup();
+                }
+            }
+        });
     }
 }
 mymap.on('click', onMapClick);
@@ -52,6 +67,9 @@ function addEvent(){
     let consigne = document.createElement("p");
     consigne.textContent = "Cliquez sur la carte pour ajouter un evenement";
     document.body.appendChild(consigne);
+    setTimeout(function(){
+        consigne.textContent = "";
+    },4000);
     click = true;
 }
 
