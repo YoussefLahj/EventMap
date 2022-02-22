74ce9b1f8689d458b0b6cfd46af068a354102b32
Samuel Haselvander <samuelhaselvander33@gmail.com>
2022-02-22 16:40:46 +0100

    add Delete and Participate

--- js/event.js
+++ js/event.js
@@ -5,8 +5,11 @@ let menuIcon = document.getElementById("Menu_icon");
 let markerTab = [];
 let titleTab = [];
 let descTab = [];
+let nbParticipantTab = [];
 let firstEnter = true;
 let click = false;
+let clickToDell = false;
+let clickToBe = false;
 var mymap = L.map('mapid').setView([47.46667, -0.55], 13);
 var popup = L.popup();
 var marker = 0;
@@ -23,68 +26,111 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 function onMapClick(e) {
     let title;
     let desc;
-    if(click){
+    if (click) {
         marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);   //marker.latlng.lat
         markerTab.push(marker);
         title = window.prompt("Saisissez un titre à votre évènement");
         titleTab.push(title);
         desc = window.prompt("Saisissez la description de votre évènement");
         descTab.push(desc);
+        nbParticipantTab.push(0);
     }
-    if(marker != 0){
-        marker.addEventListener("click", function(){
+    if (marker != 0) {
+        var index = 0;
+        marker.addEventListener("click", function () {
             console.log("marker clicked");
-            for(let i = 0; i < markerTab.length; i++){
-                if(e.latlng.lat == markerTab[i]._latlng.lat && e.latlng.lng == markerTab[i]._latlng.lng){
-                    markerTab[i].bindPopup("<b>"+titleTab[i]+"</b><br>"+descTab[i]+".").openPopup();
+            for (let i = 0; i < markerTab.length; i++) {
+                if (e.latlng.lat == markerTab[i]._latlng.lat && e.latlng.lng == markerTab[i]._latlng.lng) {
+                    markerTab[i].bindPopup("<b>" + titleTab[i] + "</b><br>" + descTab[i] + "." + "<br>Nombre de personne inscrite : " + nbParticipantTab[i]).openPopup();
+                    index = i;
                 }
+            }
+
+            if (clickToDell) {
+                markerTab[index].remove();
+                markerTab.splice(index, 1);
+                titleTab.splice(index, 1);
+                descTab.splice(index, 1);
+                nbParticipantTab.splice(index, 1);
+                clickToDell = false;
+            }
+
+            if(clickToBe){
+                //console.log(nbParticipantTab[index]);
+                nbParticipantTab[index]+=1;
+                //console.log(nbParticipantTab[index]);
             }
         });
     }
 }
 mymap.on('click', onMapClick);
 
-function openMenu(){
+function openMenu() {
     document.getElementById("Menu_Lateral").style.width = "15vw";
     document.getElementById("mainPage").style.marginLeft = "15vw";
-    
-    if(firstEnter){
+
+    if (firstEnter) {
         firstEnter = false;
         menuIcon.classList.add("fadeOut");
     }
-    else{
+    else {
         menuIcon.classList.replace("fadeIn", "fadeOut");
     }
     menuIcon.style.opacity = 0;
 }
 
-function closeMenu(){
+function closeMenu() {
     document.getElementById("Menu_Lateral").style.width = "0";
     document.getElementById("mainPage").style.marginLeft = "0";
     menuIcon.classList.replace("fadeOut", "fadeIn");
     menuIcon.style.opacity = 1;
     click = false;
+    clickToDell = false;
+    clickToBe=false;
 }
 
-function addEvent(){
+function addEvent() {
     let consigne = document.createElement("p");
     consigne.textContent = "Cliquez sur la carte pour ajouter un evenement";
     document.body.appendChild(consigne);
-    setTimeout(function(){
+    setTimeout(function () {
         consigne.textContent = "";
-    },4000);
+    }, 4000);
     click = true;
+    clickToDell = false;
+    clickToBe=false;
 }
 
-function delEvent(){
-    console.log("del");
+function delEvent() {
+    /*let Menu_Lateral=document.getElementById("Menu_Lateral");
+    let btn_Del=document.getElementById("Menu_Lateral");
+    let btn_Done = document.createElement("button");
+    btn_Done.innerHTML="Quitter";
+    Menu_Lateral.insertBefore(btn_Done,btn_Del);*/
+
+    let indication = document.createElement("p");
+    indication.textContent = "Cliquez sur l'evenement que vous voulez supprimer";
+    document.body.appendChild(indication);
+    setTimeout(function () {
+        indication.textContent = "";
+    }, 4000);
+    clickToDell = true;
+    click = false;
+    clickToBe=false;
 }
 
-function participateEvent(){
-    console.log("participate");
+function participateEvent() {
+    let indication = document.createElement("p");
+    indication.textContent = "Cliquez sur l'evenement auquel vous voulez participer";
+    document.body.appendChild(indication);
+    setTimeout(function () {
+        indication.textContent = "";
+    }, 4000);
+    clickToBe=true;
+    clickToDell = false;
+    click = false;
 }
 
-function disconnect(){
-    localStorage.setItem("connected","");
-    window.location.href="main.html"
+function disconnect() {
+    console.log("disconnect");
 }
